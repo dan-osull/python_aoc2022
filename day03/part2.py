f97ca6920e07db58b6936a397ca4abe9a9492741
Dan O'Sullivan <dan@osull.com>
2022-12-03 13:47:10 -0600

    Fix var name

--- day03/part2.py
+++ day03/part2.py
@@ -7,7 +7,7 @@ lines_gen = (line.strip() for line in data)
 
 total = 0
 # Lines are in groups of 3
-for i in range(int(len(data) / 3)):
+for _ in range(int(len(data) / 3)):
     # Get 3 lines from generator
     overlap = (
         set(next(lines_gen)).intersection(next(lines_gen)).intersection(next(lines_gen))
