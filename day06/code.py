88ef8c9f0176c8683ee4ed90a17a9a609b6b10bc
Dan O'Sullivan <dan@osull.com>
2022-12-06 11:44:17 -0600

    Day 6

---
+++ day06/code.py
@@ -0,0 +1,18 @@
+UNIQUE_CHARS_NEEDED = 14
+
+
+def main() -> None:
+    with open("day06/data.txt") as f:
+        data = f.read()
+        current_idx = UNIQUE_CHARS_NEEDED
+        while True:
+            chars_found = data[current_idx - UNIQUE_CHARS_NEEDED : current_idx]
+            if len(set(chars_found)) == UNIQUE_CHARS_NEEDED:
+                break
+            current_idx += 1
+
+        print(current_idx)
+
+
+if __name__ == "__main__":
+    main()
