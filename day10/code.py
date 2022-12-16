7356c2c5ba9ef506f05def03aa21655d821decae
Dan O'Sullivan <dan@osull.com>
2022-12-16 11:35:31 -0600

    Day 10 Part 2

--- day10/code.py
+++ day10/code.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
 
-CYCLES_OF_INTEREST = [20, 60, 100, 140, 180, 220]
+PART_ONE_CYCLES = [20, 60, 100, 140, 180, 220]
+PART_TWO_ROW_LENGTH = 40
+
 RegisterHistory = dict[int, int]
 """Key = register.cycle, value = register.value"""
+SpriteMap = dict[int, bool]
+"""Key = register.cycle, value = sprite True/False"""
 
 
 @dataclass
@@ -38,8 +42,6 @@ class Register:
 
     def advance_cycle(self) -> None:
         self._current_cycle += 1
-
-    def refresh_value(self) -> None:
         if not self.locked:
             self._current_value = self._future_value
 
@@ -54,32 +56,54 @@ def save_register_data(
     return saved_register_data
 
 
-def main_loop(
+def test_sprite_hit(register: Register) -> bool:
+    sprite_position = register.cycle
+    while sprite_position > PART_TWO_ROW_LENGTH:
+        sprite_position -= PART_TWO_ROW_LENGTH
+    if register.value <= sprite_position <= register.value + 2:
+        return True
+    return False
+
+
+def part_one_loop(
     data: list[str], register: Register, cycles_of_interest: list[int]
 ) -> RegisterHistory:
     register_history: RegisterHistory = {}
+    save_register_data(register_history, register, cycles_of_interest)
 
-    for next_command in data:
-        save_register_data(register_history, register, cycles_of_interest)
-        split = next_command.split()
+    for command in data:
+        split = command.split()
         if len(split) == 1:
             register.noop()
         else:
             register.addx(int(split[1]))
         while register.locked:
             register.advance_cycle()
-            register.refresh_value()
             save_register_data(register_history, register, cycles_of_interest)
 
     return register_history
 
 
-def part_one() -> None:
-    with open("day10/data.txt") as file:
-        data = [line.strip() for line in file.readlines()]
+def part_two_loop(data: list[str], register: Register) -> SpriteMap:
+    sprite_map: SpriteMap = {}
 
+    sprite_map[register.cycle] = test_sprite_hit(register)
+    for command in data:
+        split = command.split()
+        if len(split) == 1:
+            register.noop()
+        else:
+            register.addx(int(split[1]))
+        while register.locked:
+            register.advance_cycle()
+            sprite_map[register.cycle] = test_sprite_hit(register)
+
+    return sprite_map
+
+
+def part_one_solution(data: list[str]) -> None:
     register = Register()
-    register_history = main_loop(data, register, CYCLES_OF_INTEREST)
+    register_history = part_one_loop(data, register, PART_ONE_CYCLES)
 
     total = 0
     for key, value in register_history.items():
@@ -87,5 +111,23 @@ def part_one() -> None:
     print(total)
 
 
+def part_two_solution(data: list[str]) -> None:
+    register = Register()
+    sprite_map = part_two_loop(data, register)
+    counter = 0
+    for sprite in sprite_map.values():
+        if sprite:
+            print("#", end="")
+        else:
+            print(".", end="")
+        counter += 1
+        if counter == PART_TWO_ROW_LENGTH:
+            print()
+            counter = 0
+
+
 if __name__ == "__main__":
-    part_one()
+    with open("day10/data.txt") as file:
+        data = [line.strip() for line in file.readlines()]
+    part_one_solution(data)
+    part_two_solution(data)
--- tests/test_day10.py
+++ tests/test_day10.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day10.code import Register, main_loop
+from day10.code import Register, part_one_loop
 
 
 @pytest.fixture
@@ -13,5 +13,5 @@ def test_data():
 
 
 def test_main_loop(test_data: list[str]):
-    register_history = main_loop(test_data, Register(), [1, 2, 3, 4, 5, 6])
+    register_history = part_one_loop(test_data, Register(), [1, 2, 3, 4, 5, 6])
     assert register_history == {1: 1, 2: 1, 3: 1, 4: 4, 5: 4, 6: -1}
