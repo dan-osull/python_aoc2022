53f547caf163f76b98e19bf0ce63bde6b2cbaf81
Dan O'Sullivan <dan@osull.com>
2022-12-20 12:41:19 -0600

    Function -> classmethod

--- day11/code.py
+++ day11/code.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 from dataclasses import dataclass, field
-from typing import Literal
+from typing import Literal, Self
 
 
 @dataclass
@@ -11,6 +11,18 @@ class MonkeyCollection:
 
     def get_by_id(self, id_: int) -> Monkey:
         return next(item for item in self.monkeys if item.id_ == id_)
+
+    @classmethod
+    def from_input_data(cls, data: list[str]) -> Self:
+        def divide_into_chunks(data: list, size: int):
+            for i in range(0, len(data), size):
+                yield data[i : i + size]
+
+        collection = MonkeyCollection()
+        chunks = divide_into_chunks(data, 6)
+        for chunk in chunks:
+            collection.monkeys.append(Monkey.from_input_data(chunk))
+        return collection
 
 
 @dataclass
@@ -24,10 +36,9 @@ class Monkey:
     target_if_false: int
     items_inspected: int = 0
 
-    def process_items(self, collection: MonkeyCollection) -> None:
+    def inspect_items(self, collection: MonkeyCollection) -> None:
         for item in self.items:
             self.items_inspected += 1
-            # Item value is current worry level
             new_value = self.get_new_item_value(item)
             if new_value % self.divisible_test_number == 0:
                 target_id = self.target_if_true
@@ -36,16 +47,16 @@ class Monkey:
             collection.get_by_id(target_id).items.append(new_value)
         self.items = []
 
-    def get_new_item_value(self, item: int) -> int:
-        item_two = (
-            self.operation_value if isinstance(self.operation_value, int) else item
+    def get_new_item_value(self, current_value: int) -> int:
+        operand = (
+            current_value if self.operation_value == "old" else self.operation_value
         )
         if self.operation_type == "+":
-            return int((item + item_two) / 3)
-        return int((item * item_two) / 3)
+            return int((current_value + operand) / 3)
+        return int((current_value * operand) / 3)
 
     @classmethod
-    def from_input_data(cls, data: list[str]) -> Monkey:
+    def from_input_data(cls, data: list[str]) -> Self:
         id_ = next(int(char) for char in data[0] if char.isdigit())
         items = [
             int(item) for item in data[1].replace("Starting items: ", "").split(",")
@@ -66,29 +77,16 @@ class Monkey:
         )
 
 
-def divide_into_chunks(data: list, size: int):
-    for i in range(0, len(data), size):
-        yield data[i : i + size]
-
-
-def parse_input(data: list[str]) -> MonkeyCollection:
-    collection = MonkeyCollection()
-    chunks = divide_into_chunks(data, 6)
-    for chunk in chunks:
-        collection.monkeys.append(Monkey.from_input_data(chunk))
-    return collection
-
-
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
     with open("day11/data.txt") as file:
         data = [line.strip() for line in file.readlines() if line.strip()]
-    collection = parse_input(data)
+    collection = MonkeyCollection.from_input_data(data)
     for round in range(20):
         logging.info(f"{round=}")
         for monkey in collection.monkeys:
-            monkey.process_items(collection)
-    items_inspected = [item.items_inspected for item in collection.monkeys]
+            monkey.inspect_items(collection)
+    items_inspected = [monkey.items_inspected for monkey in collection.monkeys]
     top_two = sorted(items_inspected, reverse=True)[:2]
     print(top_two[0] * top_two[1])
 
